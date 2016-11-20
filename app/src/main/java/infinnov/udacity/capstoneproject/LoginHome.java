81bb3ba73816c8ba6bf0213f4c3c47c91171e043
priyank <mr.priyankagrawal@gmail.com>
2016-11-20 17:14:10 +0530

    picassso

--- app/src/main/java/infinnov/udacity/capstoneproject/LoginHome.java
+++ app/src/main/java/infinnov/udacity/capstoneproject/LoginHome.java
@@ -9,6 +9,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -16,17 +19,30 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginHome extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     GoogleApiClient mGoogleApiClient;
     final static int RC_SIGN_IN = 1;
+
+    @BindView(R.id.ivPlayerImage)    ImageView ivPlayerImage;
+    @BindView(R.id.tvHighScore)    TextView tvHighScore;
+    @BindView(R.id.btnPlay)    Button btnPlay;
+    @BindView(R.id.btnLeaderboard)    Button btnLeaderboard;
+    @BindView(R.id.sign_in_button)    com.google.android.gms.common.SignInButton sign_in_button;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
@@ -35,13 +51,19 @@ public class LoginHome extends AppCompatActivity implements GoogleApiClient.OnCo
                 .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
-        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+        sign_in_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 signIn();
             }
         });
-
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent3 = new Intent(getApplicationContext(), Game.class);
+                startActivity(intent3);
+            }
+        });
     }
 
     private void signIn() {
@@ -65,6 +87,9 @@ public class LoginHome extends AppCompatActivity implements GoogleApiClient.OnCo
         if (result.isSuccess()) {
             // Signed in successfully, show authenticated UI.
             GoogleSignInAccount acct = result.getSignInAccount();
+            Log.d("Nuances App", "--------------------------"+acct.getPhotoUrl());
+            //ivPlayerImage.setImageResource();
+            Picasso.with(this.getApplicationContext()).load(acct.getPhotoUrl()).into(ivPlayerImage);
             //mStatusTextView.setText(getString(R.string.signedin_fmt, acct.getDisplayName()));
             //updateUI(true);
         } else {
